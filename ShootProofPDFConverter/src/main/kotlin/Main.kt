9431f4e4078e7949d675f2b0519605c45455a855
elliot <elliotfisk@gmail.com>
2023-09-30 13:21:54 -0700

    first photos complete

--- ShootProofPDFConverter/src/main/kotlin/Main.kt
+++ ShootProofPDFConverter/src/main/kotlin/Main.kt
@@ -2,11 +2,13 @@ import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.Loader
 import org.apache.pdfbox.text.PDFTextStripper
 import java.io.*
+import kotlin.system.exitProcess
+
 fun main(args: Array<String>) {
     val pdfFile: File = File("C:\\Users\\ellio\\Downloads\\Input.pdf")
     val pdfText = GetPdfText(pdfFile)
-    val csvContent = ProcessRawText(pdfText)
-    WriteCsv(csvContent)
+    val orders = ProcessRawText(pdfText)
+    WriteCsv(orders)
 }
 
 fun GetPdfText(pdfFile: File): List<String> {
@@ -51,75 +53,66 @@ fun GetPdfText(pdfFile: File): List<String> {
 
 fun ProcessRawText(pdfList: List<String>): List<Order>{
     val zipcodeRegex = Regex("[^,]+(,[^,]+)+(,[^,]+)+[\\d\\d\\d\\d\\d]")
-    val sizeRegex = Regex("[2.5 x 3.5 (8 Wallets)]|[4 X 6]|[5 X 7]|[8 X 10]|[11 X 14]|[All Photos from this album]|[Single Photo]]")
+    val sizeRegex = Regex("(2\\.5 x 3\\.5 \\(8 Wallets\\)|4 x 6|5 x 7|8 x 10|11 x 14|All Photos from this album|Single Photo)")
     var orders = ArrayList<Order>()
     var i = 0
     var currentOrder: Order = Order(-1)
     while(i < (pdfList.size)){
         if(pdfList[i].startsWith("Order #")){
-            val orderNum = Integer.valueOf(pdfList[i].substringAfter("#").replace("\r", "").plus(","))
+            val orderNum = Integer.valueOf(pdfList[i].substringAfter("#").replace("\r", ""))
             currentOrder = Order(orderNum)
             orders.add(currentOrder)
         }
         if(pdfList[i].startsWith("Gallery Name")){
-            val galleryName = pdfList[i].substringAfter(":").replace("\r", "").plus(",")
+            val galleryName = pdfList[i].substringAfter(":").replace("\r", "")
             currentOrder.galleryName = galleryName
         }
         if(pdfList[i].startsWith("Billing Address")){
-            val nameBillingAddress = pdfList[i+1].replace("\r", "").plus(",")
+            val nameBillingAddress = pdfList[i+1].replace("\r", "")
             currentOrder.nameBillingAddress = nameBillingAddress
         }
-        if(zipcodeRegex.find(pdfList[i]) != null){
-            i++
-            var firstImage = pdfList[i].replace("\r", "")
-            while(!pdfList[i].contains(".jpg")) {
+        if(pdfList[i].startsWith("Buyer Shipping Address")){
+            while(zipcodeRegex.find(pdfList[i]) == null){
                 i++
-                //if image name line does not contain photo type
-                if (pdfList[i].contains(".jpg")) {
-                    firstImage = firstImage.plus(pdfList[i]).substringBefore(".jpg").plus(".jpg,")
-                    currentOrder.fileOrdered = firstImage
-                }
             }
+            i++
+            var firstImage = pdfList[i].substringBefore(".jpg").replace("\r", "")
+            if(!pdfList[i].contains(".jpg")) {
+                i++
+                firstImage = firstImage.plus(pdfList[i].substringBefore(".jpg").replace("\r", ""))
+            }
+            firstImage = firstImage.plus(".jpg")
+            currentOrder.fileOrdered = firstImage
             //find size ordered
-            var matchResult: MatchResult?
-            do {
+            var matchResult: MatchResult? = sizeRegex.find(pdfList[i])
+            while(matchResult == null) {
                 i++
                 matchResult = sizeRegex.find(pdfList[i])
-            } while(matchResult == null)
+            }
             val matchedText = matchResult.value
             currentOrder.sizeOrdered = matchedText
             //find quantity and total price
-
-            //get subsequent images
-            var noMoreImages = false
-            while(!noMoreImages){
-                i+=6
-                var subImage = pdfList[i].replace("\r", "")
-                while(!pdfList[i].contains(".jpg")) {
-                    i++
-                    if(pdfList[i].endsWith(".jpg\r")){
-                        subImage = subImage.plus(pdfList[i].replace("\r", ""))
-                    }
-                }
-
+            while(!pdfList[i].contains("Qty Item Price Total Price")){
+                i++
             }
+            i++
+            currentOrder.quantity = Integer.valueOf(pdfList[i].substringBefore(" "))
+            currentOrder.totalPrice = pdfList[i].substringAfterLast(" ").replace("\r", "")
         }
+        i++
     }
     return orders
 }
 
-fun WriteCsv(csvContent: List<List<String>>) {
+fun WriteCsv(orders: List<Order>) {
     try{
         val newCsv = File("C:\\Users\\ellio\\OneDrive\\Documents\\Output.csv")
         val fw = FileWriter(newCsv)
         val writer = BufferedWriter(fw)
 
         writer.write("Order#,GalleryName,NameBillingAddress,FilesOrdered,SizeOrdered,PackageCode,Quantity,Total Price,Package,\n")
-        for(i in 0 until (csvContent.size)){
-            for(j in 0 until (csvContent[i].size)) {
-                writer.write(csvContent[i][j])
-            }
-            writer.write("\n")
+        for(i in 0 until (orders.size)){
+            writer.write(orders[i].convertToCSV())
         }
         writer.close()
     }
--- ShootProofPDFConverter/src/main/kotlin/Order.kt
+++ ShootProofPDFConverter/src/main/kotlin/Order.kt
@@ -1,19 +1,20 @@
 class Order(orderNumber: Int){
 
-    val orderNumber = orderNumber
-    var galleryName = ""
-    var nameBillingAddress = ""
-    var fileOrdered = ""
-    var sizeOrdered = ""
-    private var packageCode = ""
-    var quantity = 0
-    var totalPrice = 0
-    var aPackage = ""
+    val orderNumber: Int = orderNumber
+    var galleryName: String  = ""
+    var nameBillingAddress: String  = ""
+    var fileOrdered: String  = ""
+    var sizeOrdered: String = ""
+    private var packageCode: String? = ""
+    var quantity: Int = 0
+    var totalPrice: String = ""
+    var aPackage: String  = ""
 
-    val packageCodes = mapOf("2.5 x 3.5 (8 Wallets)" to 'A', "4 X 6" to 'B', "5 X 7" to 'C', "8 X 10" to 'D', "11 X 14" to 'E', "All Photos from this album" to 'F', "Single Photo" to 'G')
+    val packageCodes = mapOf("2.5 x 3.5 (8 Wallets)" to "A", "4 x 6" to "B", "5 x 7" to "C", "8 x 10" to "D", "11 x 14" to "E", "All Photos from this album" to "F", "Single Photo" to "G")
 
-    fun convertToCSV(){
-
+    fun convertToCSV(): String{
+        packageCode = packageCodes[sizeOrdered]
+        return "$orderNumber,$galleryName,$nameBillingAddress,$fileOrdered,$sizeOrdered,$packageCode,$quantity,$totalPrice,$aPackage\n"
     }
 
     fun cloneForFile(newFileOrdered: String): Order{
